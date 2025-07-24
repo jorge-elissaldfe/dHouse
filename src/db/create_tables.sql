19ba922869776e03618b6168dd023b561b5e926e
jorge-elissaldfe <elissalde.j.e@gmail.com>
2025-07-23 21:02:46 -0300

    Add files via upload

--- src/db/create_tables.sql
+++ src/db/create_tables.sql
@@ -1,1 +1,9 @@
-
+sqlite3 dhouse.db << EOF
+CREATE TABLE if not exists log(id integer primary key, 
+		 date TEXT, 
+		 device text, 
+		 command text, 
+		 data text, 
+		 source text default '', 
+		 friendlyName text);
+EOF
