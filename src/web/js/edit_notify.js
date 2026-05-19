40f88d178a6e6d4c29b3fad2915032be79c08a88
Jorge Elissalde <elissalde.j.e@gmail.com>
2026-05-19 08:27:42 -0300

    time-based notification
    Notify "Set On" message if the event is happening in the time range

--- src/web/js/edit_notify.js
+++ src/web/js/edit_notify.js
@@ -12,7 +12,8 @@ class notifyClass {
 		{ key: 'Online',   switchId: 'online_switch',  textId: 'online_text' },
 		{ key: 'Offline',  switchId: 'offline_switch', textId: 'offline_text' },
 		{ key: 'ON',  	   switchId: 'poweron_switch', textId: 'power_on' },
-		{ key: 'OFF', 	   switchId: 'poweroff_switch', textId: 'power_off' }
+		{ key: 'OFF', 	   switchId: 'poweroff_switch', textId: 'power_off' },
+		{ key: 'TimeBased',switchId: 'time_based_switch', textId: null }
 	];
 
 	constructor() {
@@ -25,7 +26,31 @@ class notifyClass {
 		if (data!=="") 
 			this.notifyData = JSON.parse(data);
 		this.fillCurrentDevice();
+		this.fillTimeBased();
 	}	
+
+	fillCurrentDevice() {
+		this.notifyTypes.forEach(({ key, switchId, textId }) => {
+			const config = this.notifyData[device]?.[key] ?? {};
+			$(switchId).checked = config.enable === 'yes';
+			if (textId != null)
+				$(textId).value = config.message ?? '';
+		});
+	}
+
+	fillTimeBased() {
+		// console.log(device);
+		//console.log (this.notifyData[device]?.["from_hour"]);
+
+		if (this.notifyData[device]?.["from_hour"])
+			$("from_time_based_hour").value = this.notifyData[device]["from_hour"];
+		if (this.notifyData[device]?.["from_min"])
+			$("from_time_based_min").value = this.notifyData[device]["from_min"];
+		if (this.notifyData[device]?.["to_hour"])
+			$("to_time_based_hour").value = this.notifyData[device]["to_hour"];
+		if (this.notifyData[device]?.["to_min"])
+			$("to_time_based_min").value = this.notifyData[device]["to_min"];
+	}
 
 	async loadNotify() {
 		try {
@@ -45,7 +70,7 @@ class notifyClass {
 		// get messages to send
 		let jsonArray = {};
 		this.notifyTypes.forEach(({ key, switchId, textId }) => {
-	  		if ($(switchId).checked) {
+	  		if (textId!=null && $(switchId).checked) {
 				console.log ("checked: " + switchId + " text: " + textId);
     			jsonArray[key.toLowerCase()] = $(textId).value;
   			}
@@ -67,27 +92,26 @@ class notifyClass {
 		go_url(`control_device.php?device=${device}`);
 	}
 
-	fillCurrentDevice() {
-		this.notifyTypes.forEach(({ key, switchId, textId }) => {
-			const config = this.notifyData[device]?.[key] ?? {};
-				$(switchId).checked = config.enable === 'yes';
-				$(textId).value = config.message ?? '';
-		});
-	}
-
 	async saveDevice() {
 		this.notifyData[device] = {};
-		['Online', 'Offline', 'ON', 'OFF'].forEach(key => {
+		['Online', 'Offline', 'ON', 'OFF','TimeBased'].forEach(key => {
 			this.notifyData[device][key] = {};
 		});
 
 		this.notifyData[device] = {};
 		this.notifyTypes.forEach(({ key, switchId, textId }) => {
 			if (!this.notifyData[device][key]) 
-					this.notifyData[device][key] = {};
+				this.notifyData[device][key] = {};
   			this.notifyData[device][key]["enable"] = $(switchId).checked ? "yes" : "no";
-  			this.notifyData[device][key]["message"] = $(textId).value;
+			if (textId)
+  				this.notifyData[device][key]["message"] = $(textId).value;
 		});
+
+		// time based options
+		this.notifyData[device]["from_hour"] = $("from_time_based_hour").value;
+		this.notifyData[device]["from_min"] = $("from_time_based_min").value;
+		this.notifyData[device]["to_hour"] = $("to_time_based_hour").value;
+		this.notifyData[device]["to_min"] = $("to_time_based_min").value;
 
 		const result = await storeNotifyInServer(this.notifyData);
 		if (result != 'store: done') 
@@ -108,12 +132,27 @@ class notifyClass {
 		$('save-button')?.addEventListener("click", () => this.saveDevice());
 		$('close-button')?.addEventListener("click", () => this.onButtonClose());
 	}
+
+	setupTimeBased() {
+		let from = $("from_time_based");
+		let to = $("to_time_based");
+		let sendNotification = false;
+
+		$('time_based_switch').checked = sendNotification;
+  		from.appendChild(createSelect("from_time_based_hour", 40, 0, 23, "h", 0));
+		from.appendChild(document.createTextNode(":"));
+		from.appendChild(createSelect("from_time_based_min", 45, 0, 59, "m", 0));
+  		to.appendChild(createSelect("to_time_based_hour", 40, 0, 23, "h", 0));
+		to.appendChild(document.createTextNode(":"));
+		to.appendChild(createSelect("to_time_based_min", 45, 0, 59, "m", 0));
+	}
 };
 
 // called from init_page after configuration is retrieved
 function startPage() {
 	$("main_title").innerHTML = config.dHouse.devices[device]?.FriendlyName ?? device;
 	notify.startMqttConnection();
+	notify.setupTimeBased();
 }
 
 document.addEventListener("DOMContentLoaded", async function() {
